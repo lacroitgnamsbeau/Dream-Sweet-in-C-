04ad1d4b16d208a255c77725e22a86e4f973feb3
Calvin Pugay <calvinpugay@gmail.com>
2024-11-09 18:16:05 -0900

    start
    to my thing

--- introduction.cpp
+++ introduction.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
+void viewStats();
+void viewCompleted();
+void shop();
+void newTask();
 
-int main() {
-    cout << "Womp womp womp";
-    cout << "ggs cookeds";
+int main() // (view stats, view completed tasks, [casino], shop, make a new task)
+{
+    string newTask = "";
+    
+    cout << "TASK-TRACK-RPG\n";
+    cout << "1.  View Your Stats\n";
+	cout << "2.  View Tasks Completed\n";
+	cout << "3.  Visit the Shop\n";
+    cout << "4.  Create a New Task.\n";
+    cout << "5.  Exit the Program\n";
+    cout << "Enter your choice(1-5):  ";
 
 
     return 0;
