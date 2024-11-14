d693b8f7b32d64e2a0a5bea8c916105101ca8301
Calvin Pugay <calvinpugay@gmail.com>
2024-11-13 20:59:17 -0900

    Rough ideas and pseudocode
    Rough ideas, if we end up not using SDL2

--- introduction.cpp
+++ introduction.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
-#include <chrono>
+#include <vector>
+#include <random>
+#include <ctime>
 using namespace std;
 
-void viewStats();
+void viewStats(int numStats, int attributes[]);
 void viewCompleted();
 void shop();
 void createTask();
+void casino();
 void menu();
-int getChoice(); 
+void doTask();
+int getChoice(); // these definitions are subject to change, function types or arguments will probably change
 
-int main() // (view stats, view completed tasks, [casino], shop, make a new task)
+int main() // (view stats, view completed tasks, [casino], shop, make a new task, do a task)
 {
-    int choice;
+    const int ATT = 5, MAX_STAT = 100; // these can be changed if want
+    int choice, money, attributes[ATT] = {0};
+    vector<string> AllTasks;
     string newTask = "";
     	do
 	{
@@ -22,7 +28,7 @@ int main() // (view stats, view completed tasks, [casino], shop, make a new task
 
 		switch (choice)
 		{
-		case 1: viewStats();
+		case 1: viewStats(ATT, attributes);
 			break;
 
 		case 2: viewCompleted();
@@ -34,11 +40,19 @@ int main() // (view stats, view completed tasks, [casino], shop, make a new task
 		case 4: createTask();
 			break;
 
+        case 5: casino();
+            break;
+
+        case 6: doTask();
+            break;
+
 		default:
 			break;
 		}
 
-	} while (choice != 5);
+	} while (choice != 7);
+    cout << "Thanks for playing!";
+    return 0;
 
 }
 void menu()
@@ -48,8 +62,10 @@ void menu()
 	cout << "2.  View Tasks Completed.\n";
 	cout << "3.  Visit the Shop.\n";
     cout << "4.  Create a New Task.\n";
-    cout << "5.  Exit the Program.\n";
-    cout << "Enter your choice(1-5):  ";
+    cout << "5.  Visit the Casino\n";
+    cout << "6.  Attempt a Task\n";
+    cout << "7.  Exit the Program.\n";
+    cout << "Enter your choice(1-7):  ";
 }
 int getChoice()
 {
@@ -62,7 +78,7 @@ int getChoice()
         {
             valid = 0;
         }
-        else if (!(number[0] >= '1' && number[0] <= '5'))
+        else if (!(number[0] >= '1' && number[0] <= '7'))
         {
             valid = 0;
         }
@@ -72,25 +88,71 @@ int getChoice()
         }
         else
         {
-            cout << "Invalid input... This must be an integer value between 1 and 5.\n";
+            cout << "Invalid input... This must be an integer value between 1 and 7.\n";
             getline(cin, number);
         }
 
     }
 }
-void viewStats()
+void viewStats(int numStats, int attributes[])
 {
-    cout << "stats\n";
+    cout << "STATS.\n";
+    for (int i = 0; i < numStats; i++)
+    {
+        cout << "Attribute " << i << ": " << attributes[i] << endl; // names for attributes will be set accordingly
+    }
+
 }
 void viewCompleted()
 {
     cout << "viewcompleted\n";
+    /* pseudocode/ideas
+        create a for loop for every element in the vector AllTasks to print the completed tasks
+    */
 }
 void shop()
 {
     cout << "shop\n";
+    /* pseudocode/ideas
+        this function will:
+            1. take arguents such as money that the character has, and the attribute[] array
+            2. display a shop of various items
+            3. asks user what to buy or exit the shop
+            4. if user wants to buy an item
+                -check if they have enough currency (complain it if not)
+                -ask if user is sure if they want to purchase it
+                -upgrade certain attributes when an item is successfully bought
+            5. Either do not display or mark items that are already bought in some way
+    */
 }
 void createTask()
 {
     cout << "createtask\n";
+    /* pseudocode/ideas
+        cin << an input
+        print out what the user typed and ask if that is what they want to enter
+        then, the user gives the task a numerical difficulty (probably 1-10 or 1-100)
+    */
+}
+void casino()
+{
+    cout << "casino\n";
+    /* pseudocode/ideas
+        this function should take money as an argument, then we can implement a 
+        game based on randomness (random number guessing, etc.)
+    */
+}
+void doTask()
+{
+    cout << "dotask\n";
+    /* pseudocode/ideas
+        this function is the core of the program, so these are some very rough ideas:
+            1. print out the contents of the string vector AllTasks in a menu (this can be its own function)
+            2. get the user to choose a task (if there are no tasks, then the user must make a new task)
+            3. we CAN track time elapsed using the clock function https://cplusplus.com/reference/ctime/clock/ 
+            4. Depending on how we determine success/failure/partial success, we can modify attributes or other values
+                - failure would lose money and/or debuff some attributes
+                - success would gain money and/or buff attributes
+
+    */
 }
